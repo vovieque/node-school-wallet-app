064873b760bb49a5086fb275cef54a9d6c4891f5
aureznik <aureznik@yamoney.ru>
2017-07-24 16:38:27 +0300

    Переписываем приложение на koa2

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const app = express();
+const Koa = require('koa');
+const app = new Koa();
+const serve = require('koa-static');
+const router = require('koa-router')();
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.send(`<!doctype html>
+router.get('/', (ctx) => {
+	ctx.body = `<!doctype html>
   <html>
     <head>
       <link rel="stylesheet" href="/style.css">
@@ -12,24 +12,52 @@ app.get('/', (req, res) => {
     <body>
       <h1>Hello Smolny!</h1>
     </body>
-  </html>`);
+  </html>`;
 });
 
-app.get('/error', (req, res) => {
-  throw Error('Oops!');
+router.get('/error', () => {
+	throw Error('Oops!');
 });
 
-app.get('/transfer', (req, res) => {
-  const { amount, from, to } = req.query;
-  res.json({
-    result: 'success',
-    amount,
-    from,
-    to
-  });
+const transferDataParams = (ctx, next) => {
+	const {amount, from, to} = ctx.request.query;
+	ctx.responseData = {
+		amount,
+		from,
+		to
+	};
+	next();
+};
+
+router.get('/transfer', transferDataParams, (ctx) => {
+		ctx.body = Object.assign({
+			result: 'success'
+		}, ctx.responseData);
+	});
+
+// error handler
+app.use(async (ctx, next) => {
+	try {
+		await next();
+	} catch (err) {
+		console.log('Error detected', err);
+		ctx.status = 500;
+		ctx.body = `Error [${err.message}] :(`;
+	}
 });
+
+// logger
+app.use(async function(ctx, next) {
+	const start = new Date();
+	await next();
+	const ms = new Date() - start;
+	console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+app.use(router.routes());
+app.use(serve('./public'));
 
 app.listen(3000, () => {
-  console.log('YM Node School App listening on port 3000!')
+	console.log('YM Node School App listening on port 3000!')
 });
 
