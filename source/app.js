89e70593c6d493efe647c1e3af2dfff235f1555d
Vladimir Rakchaev <rakchaev@yamoney.ru>
2017-08-25 15:33:50 +0300

    Remove services

--- source/app.js
+++ source/app.js
@@ -6,7 +6,6 @@ const router = require('koa-router')();
 const bodyParser = require('koa-bodyparser')();
 
 const getCardsController = require('./controllers/cards/get-cards');
-const getServicesController = require('./controllers/services/get-services');
 const createCardController = require('./controllers/cards/create');
 const deleteCardController = require('./controllers/cards/delete');
 const getTransactionsController = require('./controllers/transactions/get');
@@ -22,10 +21,7 @@ const app = new Koa();
 router.param('id', (id, ctx, next) => next());
 
 router.get('/cards/', getCardsController);
-router.get('/services/', getServicesController);
-
 router.post('/cards/', createCardController);
-
 router.delete('/cards/:id', deleteCardController);
 
 router.get('/cards/:id/transactions/', getTransactionsController);
