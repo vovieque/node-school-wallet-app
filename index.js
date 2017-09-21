1ba4343215e2f5f58f12de06a062ab3a23a8f985
Svetlana Kolosova <sekolosova@yamoney.ru>
2017-09-21 19:38:05 +0300

    Fix codestyle

--- index.js
+++ index.js
@@ -4,32 +4,31 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send(`<!doctype html>
-  <html>
-    <head>
-      <link rel="stylesheet" href="/style.css">
-    </head>
-    <body>
-      <h1>Hello Smolny!</h1>
-    </body>
-  </html>`);
+	res.send(`<!doctype html>
+	<html>
+		<head>
+			<link rel="stylesheet" href="/style.css">
+		</head>
+		<body>
+			<h1>Hello Smolny!</h1>
+		</body>
+	</html>`);
 });
 
 app.get('/error', (req, res) => {
-  throw Error('Oops!');
+	throw Error('Oops!');
 });
 
 app.get('/transfer', (req, res) => {
-  const { amount, from, to } = req.query;
-  res.json({
-    result: 'success',
-    amount,
-    from,
-    to
-  });
+	const {amount, from, to} = req.query;
+	res.json({
+		result: 'success',
+		amount,
+		from,
+		to
+	});
 });
 
 app.listen(3000, () => {
-  console.log('YM Node School App listening on port 3000!')
+	console.log('YM Node School App listening on port 3000!');
 });
-
