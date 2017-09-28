047e3e2afcca009cb036d7efc6251c171d8d9c72
Nikita Isaev <nikitaisaev@yamoney.ru>
2017-09-28 09:08:08 +0300

    Правки настроек eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,19 +13,19 @@ module.exports = {
 		'arrow-parens': [2, 'always'],
 		'no-underscore-dangle': [2, {'allowAfterThis': true}],
 		'max-len': [2, 120, 4, {'ignoreUrls': true}],
-		'no-confusing-arrow': [2, {'allowParens': true}], // optional
-
-		'object-curly-spacing': [2, 'never'], // optional
+		'no-confusing-arrow': [2, {'allowParens': true}],
+		'object-curly-spacing': [2, 'never'],
 
 		'import/no-extraneous-dependencies': 0,
 		'import/no-unresolved': 0,
 
 		'jsx-quotes': [2, 'prefer-single'],
-		'react/jsx-filename-extension': [1, {'extensions': ['.js', '.jsx'] }],
+
+		'react/jsx-filename-extension': [1, {'extensions': ['.js', '.jsx']}],
 		'react/jsx-indent': [2, 'tab'],
 		'react/jsx-indent-props': [2, 'tab'],
 		'react/jsx-closing-bracket-location': [1, 'after-props'],
-		'react/require-default-props': 0, // optional
-		'react/no-array-index-key': 0 // optional
+		'react/require-default-props': 0,
+		'react/no-array-index-key': 0
 	}
 };
